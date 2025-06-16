c40d8eb6a05c0a276f0c0971c50bd5f981d16772
kasra10 <kasrasparrow@gmail.com>
2025-06-16 22:07:16 +0330

    add a new raw form

--- DevToolBoxOne/Form1.Designer.cs
+++ DevToolBoxOne/Form1.Designer.cs
@@ -28,15 +28,20 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DevDashBoard_FRM));
 			this.Header_PNL = new System.Windows.Forms.Panel();
 			this.TabTools_TLB = new System.Windows.Forms.TabControl();
-			this.tabPage1 = new System.Windows.Forms.TabPage();
-			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.Converter_TB0 = new System.Windows.Forms.TabPage();
+			this.ShamsiDate_BTN = new System.Windows.Forms.Button();
+			this.Formatter_TB1 = new System.Windows.Forms.TabPage();
+			this.Measurement_TB2 = new System.Windows.Forms.TabPage();
+			this.Text_TB3 = new System.Windows.Forms.TabPage();
+			this.CLICommand_TB4 = new System.Windows.Forms.TabPage();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.tabPage3 = new System.Windows.Forms.TabPage();
-			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.ShamsiDateButton_TOTP = new System.Windows.Forms.ToolTip(this.components);
 			this.TabTools_TLB.SuspendLayout();
+			this.Converter_TB0.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// Header_PNL
@@ -44,7 +49,7 @@
 			this.Header_PNL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.Header_PNL.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.Header_PNL.BackColor = System.Drawing.Color.Navy;
+			this.Header_PNL.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.Header_PNL.Location = new System.Drawing.Point(19, 7);
 			this.Header_PNL.Name = "Header_PNL";
 			this.Header_PNL.Size = new System.Drawing.Size(1146, 50);
@@ -53,66 +58,103 @@
 			// TabTools_TLB
 			// 
 			this.TabTools_TLB.Alignment = System.Windows.Forms.TabAlignment.Left;
-			this.TabTools_TLB.Controls.Add(this.tabPage1);
-			this.TabTools_TLB.Controls.Add(this.tabPage2);
-			this.TabTools_TLB.Controls.Add(this.tabPage3);
-			this.TabTools_TLB.Controls.Add(this.tabPage4);
+			this.TabTools_TLB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TabTools_TLB.Controls.Add(this.Converter_TB0);
+			this.TabTools_TLB.Controls.Add(this.Formatter_TB1);
+			this.TabTools_TLB.Controls.Add(this.Measurement_TB2);
+			this.TabTools_TLB.Controls.Add(this.Text_TB3);
+			this.TabTools_TLB.Controls.Add(this.CLICommand_TB4);
+			this.TabTools_TLB.Cursor = System.Windows.Forms.Cursors.Default;
 			this.TabTools_TLB.ImeMode = System.Windows.Forms.ImeMode.NoControl;
 			this.TabTools_TLB.Location = new System.Drawing.Point(19, 63);
 			this.TabTools_TLB.Multiline = true;
 			this.TabTools_TLB.Name = "TabTools_TLB";
 			this.TabTools_TLB.SelectedIndex = 0;
+			this.TabTools_TLB.ShowToolTips = true;
 			this.TabTools_TLB.Size = new System.Drawing.Size(1143, 693);
 			this.TabTools_TLB.TabIndex = 1;
 			// 
-			// tabPage1
+			// Converter_TB0
 			// 
-			this.tabPage1.Location = new System.Drawing.Point(30, 4);
-			this.tabPage1.Name = "tabPage1";
-			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(1109, 685);
-			this.tabPage1.TabIndex = 0;
-			this.tabPage1.Text = "Converter";
-			this.tabPage1.UseVisualStyleBackColor = true;
+			this.Converter_TB0.Controls.Add(this.ShamsiDate_BTN);
+			this.Converter_TB0.Cursor = System.Windows.Forms.Cursors.Default;
+			this.Converter_TB0.Location = new System.Drawing.Point(30, 4);
+			this.Converter_TB0.Name = "Converter_TB0";
+			this.Converter_TB0.Padding = new System.Windows.Forms.Padding(3);
+			this.Converter_TB0.Size = new System.Drawing.Size(1109, 685);
+			this.Converter_TB0.TabIndex = 0;
+			this.Converter_TB0.Text = "Converter";
+			this.Converter_TB0.UseVisualStyleBackColor = true;
 			// 
-			// tabPage2
+			// ShamsiDate_BTN
 			// 
-			this.tabPage2.Location = new System.Drawing.Point(30, 4);
-			this.tabPage2.Name = "tabPage2";
-			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(1109, 685);
-			this.tabPage2.TabIndex = 1;
-			this.tabPage2.Text = "Formatter";
-			this.tabPage2.UseVisualStyleBackColor = true;
+			this.ShamsiDate_BTN.BackColor = System.Drawing.Color.White;
+			this.ShamsiDate_BTN.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.ShamsiDate_BTN.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.ShamsiDate_BTN.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.ShamsiDate_BTN.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ShamsiDate_BTN.Location = new System.Drawing.Point(30, 24);
+			this.ShamsiDate_BTN.Name = "ShamsiDate_BTN";
+			this.ShamsiDate_BTN.Size = new System.Drawing.Size(150, 150);
+			this.ShamsiDate_BTN.TabIndex = 0;
+			this.ShamsiDate_BTN.Text = "Shamsi Date";
+			this.ShamsiDateButton_TOTP.SetToolTip(this.ShamsiDate_BTN, "Converting solar date to Gregorian date and vice versa");
+			this.ShamsiDate_BTN.UseVisualStyleBackColor = false;
+			this.ShamsiDate_BTN.Click += new System.EventHandler(this.ShamsiDate_BTN_Click);
+			// 
+			// Formatter_TB1
+			// 
+			this.Formatter_TB1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.Formatter_TB1.Location = new System.Drawing.Point(30, 4);
+			this.Formatter_TB1.Name = "Formatter_TB1";
+			this.Formatter_TB1.Padding = new System.Windows.Forms.Padding(3);
+			this.Formatter_TB1.Size = new System.Drawing.Size(1109, 685);
+			this.Formatter_TB1.TabIndex = 1;
+			this.Formatter_TB1.Text = "Formatter";
+			this.Formatter_TB1.UseVisualStyleBackColor = true;
+			// 
+			// Measurement_TB2
+			// 
+			this.Measurement_TB2.Cursor = System.Windows.Forms.Cursors.Default;
+			this.Measurement_TB2.Location = new System.Drawing.Point(30, 4);
+			this.Measurement_TB2.Name = "Measurement_TB2";
+			this.Measurement_TB2.Size = new System.Drawing.Size(1109, 685);
+			this.Measurement_TB2.TabIndex = 2;
+			this.Measurement_TB2.Text = "Measurement";
+			this.Measurement_TB2.UseVisualStyleBackColor = true;
+			// 
+			// Text_TB3
+			// 
+			this.Text_TB3.Cursor = System.Windows.Forms.Cursors.Default;
+			this.Text_TB3.Location = new System.Drawing.Point(30, 4);
+			this.Text_TB3.Name = "Text_TB3";
+			this.Text_TB3.Size = new System.Drawing.Size(1109, 685);
+			this.Text_TB3.TabIndex = 3;
+			this.Text_TB3.Text = "Text";
+			this.Text_TB3.UseVisualStyleBackColor = true;
+			// 
+			// CLICommand_TB4
+			// 
+			this.CLICommand_TB4.Cursor = System.Windows.Forms.Cursors.Default;
+			this.CLICommand_TB4.Location = new System.Drawing.Point(30, 4);
+			this.CLICommand_TB4.Name = "CLICommand_TB4";
+			this.CLICommand_TB4.Size = new System.Drawing.Size(1109, 685);
+			this.CLICommand_TB4.TabIndex = 4;
+			this.CLICommand_TB4.Text = "CLI Command";
+			this.CLICommand_TB4.UseVisualStyleBackColor = true;
 			// 
 			// panel1
 			// 
 			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.panel1.BackColor = System.Drawing.Color.Navy;
+			this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.panel1.Location = new System.Drawing.Point(16, 762);
 			this.panel1.Name = "panel1";
 			this.panel1.Size = new System.Drawing.Size(1146, 50);
 			this.panel1.TabIndex = 1;
-			// 
-			// tabPage3
-			// 
-			this.tabPage3.Location = new System.Drawing.Point(30, 4);
-			this.tabPage3.Name = "tabPage3";
-			this.tabPage3.Size = new System.Drawing.Size(1109, 685);
-			this.tabPage3.TabIndex = 2;
-			this.tabPage3.Text = "Measurement";
-			this.tabPage3.UseVisualStyleBackColor = true;
-			// 
-			// tabPage4
-			// 
-			this.tabPage4.Location = new System.Drawing.Point(30, 4);
-			this.tabPage4.Name = "tabPage4";
-			this.tabPage4.Size = new System.Drawing.Size(1109, 685);
-			this.tabPage4.TabIndex = 3;
-			this.tabPage4.Text = "Text";
-			this.tabPage4.UseVisualStyleBackColor = true;
 			// 
 			// DevDashBoard_FRM
 			// 
@@ -127,12 +169,13 @@
 			this.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Margin = new System.Windows.Forms.Padding(4);
 			this.MaximizeBox = false;
 			this.Name = "DevDashBoard_FRM";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Tool Box One";
 			this.TabTools_TLB.ResumeLayout(false);
+			this.Converter_TB0.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -141,11 +184,14 @@
 
 		private System.Windows.Forms.Panel Header_PNL;
 		private System.Windows.Forms.TabControl TabTools_TLB;
-		private System.Windows.Forms.TabPage tabPage1;
-		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage Converter_TB0;
+		private System.Windows.Forms.TabPage Formatter_TB1;
 		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.TabPage tabPage3;
-		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.TabPage Measurement_TB2;
+		private System.Windows.Forms.TabPage Text_TB3;
+		private System.Windows.Forms.TabPage CLICommand_TB4;
+		private System.Windows.Forms.Button ShamsiDate_BTN;
+		private System.Windows.Forms.ToolTip ShamsiDateButton_TOTP;
 	}
 }
 
--- DevToolBoxOne/Form1.cs
+++ DevToolBoxOne/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿// Ignore Spelling: FRM
+
+using DevToolBoxOne.Forms.Converter;
+using System.Windows.Forms;
 
 namespace DevToolBoxOne
 {
@@ -7,6 +10,12 @@ namespace DevToolBoxOne
 		public DevDashBoard_FRM()
 		{
 			InitializeComponent();
+		}
+
+		private void ShamsiDate_BTN_Click(object sender, System.EventArgs e)
+		{
+			ShamsiDate_FRM ShamsiDateForm = new ShamsiDate_FRM();
+			ShamsiDateForm.ShowDialog();
 		}
 	}
 }
